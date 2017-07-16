4784cedee374706a5d7c86490f57043769261ebf
sondrelg <sondrelg@live.no>
2017-07-16 17:26:41 +0200

    Update setup.py

--- ntfscrape/setup.py
+++ ntfscrape/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='ntfscrape',
       version='0.1',
       description='Simple data scraping from https://www.netfonds.no',
-      url='',
+      url='https://github.com/sondrelg/NTFscrape',
       author='Sondre',
       author_email='sondrelg@live.no',
       license='MIT',
